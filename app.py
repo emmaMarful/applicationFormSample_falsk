b4b2f2b3e86a80c634e5a7a33d3b31ef0c944af0
emmaMarful <eamarful@gmail.com>
2023-12-08 12:14:53 +0000

    storing data and data submission notification with jinja

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -24,8 +25,18 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+
         print(f"{first_name}, {last_name}, {email}, {date}, {occupation}")
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+
+        db.session.add(form)
+        db.session.commit()
+
+        flash("Your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
